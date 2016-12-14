4f485d94b10bdccfc3e2e77fe48b7e0a2af88321
Alvaro <Alvaro@alvaro-laptop>
2016-12-14 19:02:27 -0300

    Uso de la API desde el Frontend

--- src/client/tv-shows-container/index.js
+++ src/client/tv-shows-container/index.js
@@ -8,8 +8,10 @@ var $tvShowsContainer = $('#app-body').find('.tv-shows')
 
 $tvShowsContainer.on('click', 'button.like', function (ev) {
   var $this = $(this);
-  $this.data('id')
-  $this.closest('.tv-show').toggleClass('liked')
+  var id = $this.data('id')
+  $.post('/vote/' + id, function () {
+    $this.closest('.tv-show').toggleClass('liked')
+  })  
 })
 
 export default $tvShowsContainer
